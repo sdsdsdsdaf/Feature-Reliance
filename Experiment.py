4ff7e48714c37c0b904e5cda7e4eff3b5fcf1c33
Dongchan Kim <kdch6690@gmail.com>
2026-04-17 00:06:53 +0900

    Add CKA JS,

--- Experiment.py
+++ Experiment.py
@@ -4,75 +4,84 @@ import torch
 from Utils.Config import *
 from Utils.utils import run_experiments
 
+alpha_grid = [0, 5, 10, 20, 35, 50, 80]
+sigma_grid = [0.5, 1.0, 2.0, 3.5, 6.0]
 
 if __name__ == "__main__":
 
     resnet = timm.create_model("resnet50", pretrained=True)
     vit = timm.create_model("vit_base_patch16_224.augreg_in1k", pretrained=True)
+    i = 0
+    for alpha in alpha_grid:
+        for sigma in sigma_grid:
+            i += 1
+            total_i = len(sigma_grid) * len(alpha_grid)
+            print(f"[{i}/{total_i}] alpha: {alpha}, sigma: {sigma}")
+    
+            model_specs = [
+                ModelSpec(
+                    model_name="resnet50",
+                    pretrained_weight="in1k",
+                    model=resnet,
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                ),
+                ModelSpec(
+                    model_name="vit-b",
+                    pretrained_weight="augreg_in1k",
+                    model=vit,
+                    mean=[0.5, 0.5, 0.5],
+                    std=[0.5, 0.5, 0.5],
+                ),
+            ]
 
-    model_specs = [
-        ModelSpec(
-            model_name="resnet50",
-            pretrained_weight="in1k",
-            model=resnet,
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        ),
-        ModelSpec(
-            model_name="vit-b",
-            pretrained_weight="augreg_in1k",
-            model=vit,
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5],
-        ),
-    ]
+            transform_hparams = TransformHyperParams(
+                resize_size=224,
+                p=1.0,
+                prefix="resizecrop",
+                bilateral_d=11,
+                sigma_color=170,
+                sigma_space=75,
+                gaussian_k=11,
+                gaussian_sigma=2.0,
+                gray_alpha=1.0,
+                grid_size=6,
+                alpha_localwarp=alpha,
+                sigma_localwarp=sigma,
+            )
 
-    transform_hparams = TransformHyperParams(
-        resize_size=224,
-        p=1.0,
-        prefix="resizecrop",
-        bilateral_d=11,
-        sigma_color=170,
-        sigma_space=75,
-        gaussian_k=11,
-        gaussian_sigma=2.0,
-        gray_alpha=1.0,
-        grid_size=6,
-        alpha_localwarp=35.0,
-        sigma_localwarp=2.5,
-    )
+            data_config = DataConfig(
+                batch_size=256,
+                num_workers=4,
+                pin_memory=False,
+                shuffle=False,
+                dataset_root="Data",   # 필요 없으면 None
+            )
 
-    data_config = DataConfig(
-        batch_size=256,
-        num_workers=4,
-        pin_memory=False,
-        shuffle=False,
-        dataset_root="Data",   # 필요 없으면 None
-    )
+            extraction_config = ExtractionConfig(
+                root_dir="Cache",
+                device="cuda:0" if torch.cuda.is_available() else "cpu",
+                dtype="float16",
+                overwrite=False,
+                debug_first_batch=False,
+            )
 
-    extraction_config = ExtractionConfig(
-        root_dir="Cache",
-        device="cuda:0" if torch.cuda.is_available() else "cpu",
-        dtype="float16",
-        overwrite=False,
-        debug_first_batch=False,
-    )
-
-    result:ExperimentResult = run_experiments(
-        model_specs=model_specs,
-        transform_hparams=transform_hparams,
-        data_config=data_config,
-        extraction_config=extraction_config,
-        perturbations=[
-            "original",
-            "bilateral",
-            "grayscale",
-            "patchshuffle",
-            "patchrotation",
-            "localwarp",
-        ],
-        verbose_image=True,
-        save_summary=True,
-        run_validation=False,
-        validation_max_samples=None
-    )
+            result:ExperimentResult = run_experiments(
+                model_specs=model_specs,
+                transform_hparams=transform_hparams,
+                data_config=data_config,
+                extraction_config=extraction_config,
+                perturbations=[
+                    "original",
+                    "bilateral",
+                    "grayscale",
+                    "patchshuffle",
+                    "patchrotation",
+                    "localwarp",
+                ],
+                verbose_image=True,
+                save_summary=True,
+                run_validation=False,
+                validation_max_samples=800
+            )
+            print()
